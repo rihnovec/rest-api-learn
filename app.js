d358379a7a1e6eabf2c8d716e876440836ad9333
Anton Rikhnovets <rihnovec93@mail.ru>
2021-07-30 22:23:23 +0900

    Добавляет взаимодействие с сервером

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
 
+const CONTACTS = [
+  {
+    id: v4(),
+    name: 'Антон',
+    value: '+7(914)-590-70-51',
+    marked: false
+  }
+]
 
+app.use(express.json())
+
+app.get('/api/contacts', (request, response) => {
+  response.status(200).json(CONTACTS)
+})
+
+app.post('/api/contacts', (request, response) => {
+  const contact = {...request.body, id: v4(), marked: false}
+  CONTACTS.push(contact)
+  response.status(201).json(contact)
+})
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
--- client/frontend.js
+++ client/frontend.js
@@ -1,5 +1,15 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
 
+Vue.component('loader', {
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  `
+})
+
 new Vue({
   el: '#app',
   data() {
@@ -8,14 +18,8 @@ new Vue({
         name: '',
         value: ''
       },
-      contacts: [
-        {
-          id: 1,
-          name: 'Антон',
-          value: '+7(914)-590-70-51',
-          marked: false
-        }
-      ]
+      contacts: [],
+      loading: false
     }
   },
   computed: {
@@ -24,12 +28,13 @@ new Vue({
     }
   },
   methods: {
-    createContact() {
+    async createContact() {
       const {...contact} = this.form
 
-      this.contacts.push({...contact, id: Date.now(), marked: false})
-      this.clearForm()
+      const newContact = await request('/api/contacts', 'POST', contact)
 
+      this.contacts.push(newContact)
+      this.clearForm()
     },
 
     markContact(id) {
@@ -44,5 +49,32 @@ new Vue({
     clearForm() {
       this.form.name = this.form.value = ''
     }
+  },
+  async mounted() {
+    this.loading = true
+    this.contacts = await request('/api/contacts')
+    this.loading = false
   }
 })
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {}
+    let body
+
+    if (data) {
+      headers['Content-Type'] = 'application/json'
+      body = JSON.stringify(data)
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body
+    })
+
+    return await response.json()
+  } catch (e) {
+    console.warn('Error:', e.message)
+  }
+}
